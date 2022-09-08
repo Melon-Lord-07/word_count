6b712b59ea3df6cc02a2eb59c49ffaa8dc93dd12
Melon-Lord-07 <nitchis@educ.college-montreal.qc.ca>
2022-09-08 16:10:34 -0400

    second time commiting  :3

---
+++ main.py
@@ -0,0 +1,6 @@
+#sandra nitchi
+#TP1
+
+chaine = "The zen of Python boom"
+word_count = chaine.count(" ") + 1 #le nombre de mots est le nombre d'espaces + 1
+print(word_count) #l'ordi affiche le nombre de mots
